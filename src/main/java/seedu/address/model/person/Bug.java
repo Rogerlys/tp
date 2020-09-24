bfae794d1da5a9fe850cf0f878a4cb70f5155b58
Roger <E0406757@u.nus.edu>
2020-09-24 20:47:08 +0800

    no message

--- src/main/java/seedu/address/model/person/Bug.java
+++ src/main/java/seedu/address/model/person/Bug.java
@@ -1,19 +1,18 @@
 package seedu.address.model.person;
 
-import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
-
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 import seedu.address.model.tag.Tag;
 
 /**
- * Represents a Person in the address book.
+ * Represents a Bug in the address book.
  * Guarantees: details are present and not null, field values are validated, immutable.
  */
-public class Person {
+public class Bug {
 
     // Identity fields
     private final Name name;
@@ -27,7 +26,7 @@ public class Person {
     /**
      * Every field must be present and not null.
      */
-    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
+    public Bug(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
         requireAllNonNull(name, phone, email, address, tags);
         this.name = name;
         this.phone = phone;
@@ -64,14 +63,14 @@ public class Person {
      * Returns true if both persons of the same name have at least one other identity field that is the same.
      * This defines a weaker notion of equality between two persons.
      */
-    public boolean isSamePerson(Person otherPerson) {
-        if (otherPerson == this) {
+    public boolean isSamePerson(Bug otherBug) {
+        if (otherBug == this) {
             return true;
         }
 
-        return otherPerson != null
-                && otherPerson.getName().equals(getName())
-                && (otherPerson.getPhone().equals(getPhone()) || otherPerson.getEmail().equals(getEmail()));
+        return otherBug != null
+                && otherBug.getName().equals(getName())
+                && (otherBug.getPhone().equals(getPhone()) || otherBug.getEmail().equals(getEmail()));
     }
 
     /**
@@ -84,16 +83,16 @@ public class Person {
             return true;
         }
 
-        if (!(other instanceof Person)) {
+        if (!(other instanceof Bug)) {
             return false;
         }
 
-        Person otherPerson = (Person) other;
-        return otherPerson.getName().equals(getName())
-                && otherPerson.getPhone().equals(getPhone())
-                && otherPerson.getEmail().equals(getEmail())
-                && otherPerson.getAddress().equals(getAddress())
-                && otherPerson.getTags().equals(getTags());
+        Bug otherBug = (Bug) other;
+        return otherBug.getName().equals(getName())
+                && otherBug.getPhone().equals(getPhone())
+                && otherBug.getEmail().equals(getEmail())
+                && otherBug.getAddress().equals(getAddress())
+                && otherBug.getTags().equals(getTags());
     }
 
     @Override
